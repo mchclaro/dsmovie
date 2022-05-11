c71ee60b22f3bd73befa178ec6361f3cf74a2e93
michel <michel.seve24@gmail.com>
2022-05-11 19:25:32 -0300

    Corrigindo meu Navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,22 +1,23 @@
-import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
-  return (
-    <header>
-      <nav className="container">
-        <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
-          <a href="https://github.com/mchclaro" target="_blank" rel="noreferrer">
-            <div className="dsmovie-contact-container">
-              <GithubIcon />
-              <p className="dsmovie-contact-link">/mchclaro</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
-  );
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/mch-claro">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/mch-claro</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
